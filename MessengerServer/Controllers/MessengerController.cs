4626ecadc7f47ea673e4b9574fbd257857a30c2c
Misha <dudos0241@mail.ru>
2022-10-12 02:05:26 +0300

    add server

---
+++ MessengerServer/Controllers/MessengerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MessengerServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessengerController : ControllerBase
+    {       
+        // GET api/<MessengerController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Server is running..";
+        }
+
+        // POST api/<MessengerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
